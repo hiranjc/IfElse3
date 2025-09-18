a2ec68caae3d2898f874eadde2cd723b59dd90ff
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:17:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace IfElse3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            if (x == 1)
+            {
+                day = "Sunday";
+            }
+            else if (x == 2)
+            {
+                day = "Monday";
+            }
+            else if (x == 3)
+            {
+                day = "Tuesday";
+            }
+            else if (x == 4)
+            {
+                day = "Wednesday";
+            }
+            else if (x == 5)
+            {
+                day = "Thursday";
+            }
+            else if (x == 6)
+            {
+                day = "Friday";
+            }
+            else if (x == 7)
+            {
+                day = "Saturday";
+            }
+            else
+            {
+                day = "Invalid value";
+            }
+            Console.WriteLine("Day: " + day);
+        }
+    }
+}
